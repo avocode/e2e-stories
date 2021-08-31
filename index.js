5c5e175328fb6a995f372f8a2de94ba7605aee90
frantisekkoci <efraim9@email.cz>
2021-08-31 13:59:20 +0200

    fix: if no outputType is provided, puppeteer is used.

--- index.js
+++ index.js
@@ -1,31 +1,47 @@
 const Utils = require("./build/core/utils").default
 
 const convertStories = (config = {}) => {
+  if(config.outputType) {
+    switch (config.outputType.toLowerCase()) {
+      case "puppeteer": {
+        const Core = require("./build/templates/puppeteer/core").default
+        const convertor = new Core()
+        convertor.setConfig({
+          out: config.out || './spec',
+          headers: config.headers || {},
+          path: config.pathToYaml
+        })
 
-  switch (config.outputType && config.outputType.toLowerCase()) {
-    case "puppeteer": {
-      const Core = require("./build/templates/puppeteer/core").default
-      const convertor = new Core()
-      convertor.setConfig({
-        out: config.out || './spec',
-        headers: config.headers || {},
-        path: config.pathToYaml
-      })
+        if (config.clean !== false) {
+          Utils.cleanOutputDir(config.out || './spec')
+        }
 
-      if (config.clean !== false) {
-        Utils.cleanOutputDir(config.out || './spec')
-      }
+        if (config.together) {
+          convertor.loadFilesConvertThemAndSaveTogether()
+          break
+        }
 
-      if (config.together) {
-        convertor.loadFilesConvertThemAndSaveTogether()
+        convertor.loadFilesConvertThemAndSaveSeparatelly() 
         break
       }
-
-      convertor.loadFilesConvertThemAndSaveSeparatelly() 
-      break
-
+      default: throw new Error("Invalid output type, use only ones specified in the docs.")
     }
-    default: throw new Error("Invalid output type, use only ones specified in the docs.")
+  } else {
+    const Core = require("./build/templates/puppeteer/core").default
+    const convertor = new Core()
+    convertor.setConfig({
+      out: config.out || './spec',
+      headers: config.headers || {},
+      path: config.pathToYaml
+    })
+
+    if (config.clean !== false) {
+      Utils.cleanOutputDir(config.out || './spec')
+    }
+    
+    config.together
+      ? convertor.loadFilesConvertThemAndSaveTogether()
+      : convertor.loadFilesConvertThemAndSaveSeparatelly() 
   }
 }
 
