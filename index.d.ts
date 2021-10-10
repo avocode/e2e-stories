56a2ef0c2d7af8d515173876e7041f7047ee1399
efraim9 <68015670+efraim9@users.noreply.github.com>
2021-10-11 00:54:42 +0200

    fix: together config option added to declaration file

--- index.d.ts
+++ index.d.ts
@@ -5,11 +5,12 @@ export = convertStories;
  * @param config.outputType tool which the tests are going to be using, currently only puppeteer, soon there is going to be more tools
  * @param config.headers object with headers for the tests, can be access tokens or whatever you want
  * @param config.pathToYaml glob pattern to find yaml files, be careful with it
- *
+ * @param config.together  If you want to have all your stories merged to one testing file, set this option to true. Default is false.
  */
 declare function convertStories(config?: {    
     out: string,
     outputType?: "puppeteer", 
     headers?: object, 
     pathToYaml?: string,
+    together?: boolean,
 }): void;
