96060f98a0209bc4b84309d205f406754c075495
efraim9 <68015670+efraim9@users.noreply.github.com>
2021-10-11 00:56:27 +0200

    chore: useless dotenv setup removed

--- jest-global-setup.js
+++
@@ -1,7 +0,0 @@
-const { setup: setupPuppeteer } = require('jest-environment-puppeteer')
-
-module.exports = async function globalSetup() {
-  await setupPuppeteer()
-  // Your global setup
-  require('dotenv').config()
-}
